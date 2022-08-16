725a0c7c354a77c19e9953c68e12615c6430dbc2
Absent-kai <absent.kai@gmail.com>
2022-08-17 00:20:29 +0300

    6s

--- yatube/posts/tests/test_urls.py
+++ yatube/posts/tests/test_urls.py
@@ -1,7 +1,9 @@
 from http import HTTPStatus
 
 from django.contrib.auth import get_user_model
+from django.core.cache import cache
 from django.test import Client, TestCase
+from django.urls import reverse
 
 from ..models import Group, Post
 
@@ -30,6 +32,23 @@ class StaticURLTests(TestCase):
         self.authorized_client.force_login(self.auth)
         self.authorized_no_auth = Client()
         self.authorized_no_auth.force_login(self.user)
+        cache.clear()
+
+    def test_urls_uses_correct_template(self):
+        templates_url_names = {
+            '/': 'posts/index.html',
+            f'/group/{self.group.slug}/': 'posts/group_list.html',
+            f'/profile/{self.user}/': 'posts/profile.html',
+            f'/posts/{self.post.id}/': 'posts/post_detail.html',
+            f'/posts/{self.post.id}/edit/': 'posts/create_post.html',
+            '/create/': 'posts/create_post.html',
+            '/follow/': 'posts/follow.html',
+
+        }
+        for address, template in templates_url_names.items():
+            with self.subTest(address=address):
+                response = self.authorized_client.get(address)
+                self.assertTemplateUsed(response, template)
 
     def test_pages(self):
         url_names = (
@@ -58,39 +77,3 @@ class StaticURLTests(TestCase):
         for url, redirect in urls.items():
             with self.subTest(url=url):
                 self.assertRedirects(self.guest_client.get(url), redirect)
-
-
-class TemplateTestClass(TestCase):
-    @classmethod
-    def setUpClass(cls):
-        super().setUpClass()
-        cls.user = User.objects.create_user(username='user')
-        cls.group = Group.objects.create(
-            title='test-group',
-            slug='test-slug',
-            description='test-description',
-        )
-        cls.post = Post.objects.create(
-            author=cls.user,
-            text='test-post',
-        )
-
-    def setUp(self):
-        self.client = Client()
-        self.client.force_login(self.user)
-
-    def test_urls_uses_correct_template(self):
-        templates_url_names = {
-            '/': 'posts/index.html',
-            f'/group/{self.group.slug}/': 'posts/group_list.html',
-            f'/profile/{self.user}/': 'posts/profile.html',
-            f'/posts/{self.post.id}/': 'posts/post_detail.html',
-            f'/posts/{self.post.id}/edit/': 'posts/create_post.html',
-            '/create/': 'posts/create_post.html',
-            '/follow/': 'posts/follow.html',
-
-        }
-        for address, template in templates_url_names.items():
-            with self.subTest(address=address):
-                response = self.client.get(address)
-                self.assertTemplateUsed(response, template)
